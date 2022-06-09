4591cfdbca910a2da78576375a40501a4d33205f
Olyathecute <72819725+Olyathecute@users.noreply.github.com>
2022-06-09 23:14:06 +0400

    gh-pages

--- src/components/Plates/Plates.js
+++ src/components/Plates/Plates.js
@@ -4,7 +4,7 @@ import { PlatesWrapper, Plate1, Plate2, Plate3 } from './PlatesStyels'
 export default function Plates() {
   return (
     <PlatesWrapper>
-      <Plate1 src="assets/plate5.png" />
+      <Plate1 src="../assets/plate5.png" />
       <Plate2 src="assets/plate2.png" />
       <Plate3 src="assets/plate3.png" />
     </PlatesWrapper>
