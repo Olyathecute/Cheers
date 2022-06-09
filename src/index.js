6cc154c8e64bd5b1724fcea828f4519b37394167
Olyathecute <72819725+Olyathecute@users.noreply.github.com>
2022-06-10 02:29:00 +0400

    gh-pages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import GlobalStyles from './globalStyles'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GlobalStyles />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
